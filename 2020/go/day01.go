3d35b3a5efc8f19af12575517887821d77f28dbb
Scott Ames <scott@ames.sh>
2021-04-18 18:06:08 -0700

    [go] 2020 day1

---
+++ 2020/go/day01.go
@@ -0,0 +1,74 @@
+package aoc2020
+
+import (
+	"fmt"
+	"github.com/elliotchance/pie/pie"
+	"github.com/scottames/adventofcode/pkg/helpers"
+)
+
+const twentyTwenty = 2020
+
+// Day01 - Day 1 Part 1 & 2
+func Day01() {
+	get, err := helpers.ReadInput(2020, 1)
+	helpers.ExitOnError(err)
+
+	input, err := helpers.StringSplitNewlinesToInts(get)
+	helpers.ExitOnError(err)
+
+	part1(input)
+	part2(input)
+
+}
+
+func part1(input pie.Ints) {
+	fmt.Println("--- Part One ---")
+
+	r := twoNumbersSum2020(input)
+	fmt.Println("numbers: ", r)
+	fmt.Println("product: ", r.Product())
+	fmt.Println()
+}
+
+func twoNumbersSum2020(list pie.Ints) pie.Ints {
+	var result pie.Ints
+	bar := func(i int) bool {
+		for _, k := range list {
+			if i+k == twentyTwenty {
+				return true
+			}
+		}
+		return false
+	}
+	for i := 0; i < 2; i++ {
+		result = list.Filter(bar)
+	}
+	return result
+}
+
+func part2(input pie.Ints) {
+	fmt.Println("--- Part Two ---")
+
+	r := threeNumbersSum2020(input)
+	fmt.Println("numbers: ", r)
+	fmt.Println("product: ", r.Product())
+	fmt.Println()
+}
+
+func threeNumbersSum2020(list pie.Ints) pie.Ints {
+	var result pie.Ints
+	bar := func(i int) bool {
+		for _, k := range list {
+			x := twentyTwenty - (i + k)
+			if list.Contains(x) {
+				result.Append(x)
+				return true
+			}
+		}
+		return false
+	}
+	for i := 0; i < 3; i++ {
+		result = list.Filter(bar)
+	}
+	return result
+}
